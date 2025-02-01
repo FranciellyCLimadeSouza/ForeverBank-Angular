80b045eeeb30119a40a4152eb662d89791c58051
FranciellyCLimadeSouza <f.lima.de.souza@avanade.com>
2025-02-01 20:38:13 -0300

    .

--- src/app/componentes/recuperar-senha/recuperar-senha.component.ts
+++ src/app/componentes/recuperar-senha/recuperar-senha.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // este recurso é necessário para a operação com formulários do angular
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { User } from '../../models/user'; // Este é o model que "trata" todos os dados referentes a usuários
+import { response } from 'express';
 import { HeaderHomeComponent } from '../header-home/header-home.component';
+import { LoginComponent } from '../login/login.component';
 @Component({
   selector: 'app-recuperar-senha',
   standalone: true,
-  imports: [HeaderHomeComponent],
+  imports: [FormsModule, RouterLink, CommonModule,HeaderHomeComponent],
   templateUrl: './recuperar-senha.component.html',
   styleUrl: './recuperar-senha.component.css'
 })
 export class RecuperarSenhaComponent {
+ errorMessage: string = ''
+ user: User = new User('', '', '')
 
+  constructor (private authService: AuthService, private router: Router) {}
+
+  goToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+  onSubmit(): void {
+   
+  }
 }
