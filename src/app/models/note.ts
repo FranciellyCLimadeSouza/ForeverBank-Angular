2f2cfe24d67f2480ec8814a4b7bd77c4c832a77f
clareanarodrigues <c.rodrigues.silva@avanade.com>
2025-02-04 15:32:45 -0300

    Update note.ts

--- src/app/models/note.ts
+++ src/app/models/note.ts
@@ -1,21 +1,21 @@
-import { User } from "./user";
+import { Usuario } from "./user";
 
 export class Note {
     id: number | undefined
     title: string
     content: string
     update: Date
-    user: User
+    usuario: Usuario
 
     constructor(
         title: string,
         content: string,
         update: Date,
-        user: User,
+        usuario: Usuario
     ){
         this.title = title
         this.content = content
         this.update = update
-        this.user = user
+        this.usuario = usuario
     }
 }
