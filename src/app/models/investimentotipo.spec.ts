701eb1880075f74d33b6c5084b02190fe5a48631
clareanarodrigues <c.rodrigues.silva@avanade.com>
2025-02-04 18:31:04 -0300

    Create investimentotipo.spec.ts

---
+++ src/app/models/investimentotipo.spec.ts
@@ -0,0 +1,7 @@
+import { InvestimentoTipo } from './investimentotipo';
+
+describe('InvestimentoTipo', () => {
+  it('should create an instance', () => {
+    expect(new InvestimentoTipo()).toBeTruthy();
+  });
+});
