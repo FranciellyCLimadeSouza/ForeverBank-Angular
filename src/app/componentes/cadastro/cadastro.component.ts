9b107ac5299058d9804b22d2fbfaba6b59e8c850
FranciellyCLimadeSouza <f.lima.de.souza@avanade.com>
2025-02-04 22:31:21 -0300

    .

--- src/app/componentes/cadastro/cadastro.component.ts
+++ src/app/componentes/cadastro/cadastro.component.ts
@@ -40,7 +40,7 @@ export class CadastroComponent {
   };
 
   // 3º passo: praticar a injeção de dependência - a partir do service
-  constructor (private authService: AutenticacaoService, private router: Router) {}
+  constructor (private AutenticacaoService: AutenticacaoService, private router: Router) {}
 
   // 4º passo: definir um método onSubmit(): este método será chamado quando o usuário, no final do cadastro, clicar no botão para enviar os dados para o service que, por sua vez, vai enviar os dados para o endpoint no backend para que, assim, os dados possam ser armazenados no DB.
   goToLogin(): void {
@@ -50,39 +50,19 @@ export class CadastroComponent {
     // definir uma constante para receber como valor a role referente a este usuário.
 
     const roleName = this.selectedRole
-
-    // vamos usar a role selecionada para compor o restante do conjunto de dados para serem enviados para o service
-
-   // this.authService.register(this.user, roleName)//: até este momento, chamamos o método register, que compõe o service, à sua execução
-
-    // método que "efetivamente" executa o elemento Observable
-    /*this.authService.register(this.user, roleName).subscribe({
+    this.AutenticacaoService.registro(this.usuario, roleName).subscribe({
       next: (response) => {
-        console.log('Usuário cadastrado com sucesso!', response.message)
+        console.log('Usuario cadastrado com sucesso!', response.message)
         this.router.navigate(['/login'])
       },
-
-      // Este bloco de código faz parte do tratamento de erro definido no service. Ele lida com a seguinte chamada: this.authService.register(this.user, roleName)
-
-      // error.error: é o corpo da resposta de erro. Caso ocorra, retornada pelo backend
-
-      // && error.error.length
-
-      // this.errorMessage = error[0].defaultMessage:
-      error: (error) => {
-        if(error.error && error.error.length > 0) {
-          // Exibir a mensagem de erro retornada pelo backend 
-
-          this.errorMessage = error[0].defaultMessage
-
-        } else {
-
-          this.errorMessage = 'Erro ao registrar o usuário. Tente novamente mais tarde!'
+      error: (error) =>{
+        if(error.error && error.error.length > 0){
+          // exibir a mensagem de erro retornada pelo backend
+          this.errorMessage = error.error[0].defaultMessage
+        }else{
+          this.errorMessage = 'Erro ao registrar o usuario. Tente novamente mais tarde!'
         }
       }
-      
     })
   }
-*/
-
-}}
+}
